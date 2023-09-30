c6fbbdc2eacbb73b8d8589c065f2bf29731d20a9
Francesco Filauro <francesco.filauro@isendu.com>
2023-09-30 16:25:48 +0200

    init

---
+++ src/views/HomePage.vue
@@ -0,0 +1,19 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'HomePage'
+})
+</script>
+
+<template>
+  <div class="page-container">
+    <md-app> CIAO </md-app>
+  </div>
+</template>
+
+<style scoped>
+.HomePage {
+  background-color: seagreen;
+}
+</style>
